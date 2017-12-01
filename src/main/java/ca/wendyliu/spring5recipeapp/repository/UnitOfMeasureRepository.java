5ef3a492fe767cf158c8771df4a8428bef8cf044
Wendy Liu <jingyliu@paypal.com>
2017-12-01 11:44:54 -0800

    Made classes that extend CRUDRepository so we can perform CRUD ops on Category, Recipe and UnitOfMeasure

---
+++ src/main/java/ca/wendyliu/spring5recipeapp/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,11 @@
+package ca.wendyliu.spring5recipeapp.repository;
+
+import ca.wendyliu.spring5recipeapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+}
