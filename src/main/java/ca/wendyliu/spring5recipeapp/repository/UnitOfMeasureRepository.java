b45da3250e2b3f93be15c510f1b84735b9716080
Wendy Liu <jingyliu@paypal.com>
2017-12-14 17:16:52 -0800

    [master] CRUD repo for UnitOfMeasure

--- src/main/java/ca/wendyliu/spring5recipeapp/repository/UnitOfMeasureRepository.java
+++ src/main/java/ca/wendyliu/spring5recipeapp/repository/UnitOfMeasureRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 
+    /** Spring JDBC will auto-generate this method! */
     Optional<UnitOfMeasure> findByDescription(String description);
 }
