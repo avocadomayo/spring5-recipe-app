712288b9c113286d32d437e82e858ffcc23b041f
Wendy Liu <jingyliu@paypal.com>
2017-12-28 22:25:17 -0800

    [master] Organized imports

--- src/main/java/ca/wendyliu/spring5recipeapp/controller/IndexController.java
+++ src/main/java/ca/wendyliu/spring5recipeapp/controller/IndexController.java
@@ -1,16 +1,10 @@
 package ca.wendyliu.spring5recipeapp.controller;
 
-import ca.wendyliu.spring5recipeapp.domain.Category;
-import ca.wendyliu.spring5recipeapp.domain.UnitOfMeasure;
-import ca.wendyliu.spring5recipeapp.repository.CategoryRepository;
-import ca.wendyliu.spring5recipeapp.repository.UnitOfMeasureRepository;
 import ca.wendyliu.spring5recipeapp.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
