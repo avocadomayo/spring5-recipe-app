4f87a0d3dd013ee33bb75353d902d27e96f4c0f6
Wendy Liu <jingyliu@paypal.com>
2017-12-28 22:24:42 -0800

    [master] Decoupled recipe service implementation

--- src/main/java/ca/wendyliu/spring5recipeapp/service/RecipeService.java
+++ src/main/java/ca/wendyliu/spring5recipeapp/service/RecipeService.java
@@ -1,32 +1,13 @@
 package ca.wendyliu.spring5recipeapp.service;
 
 import ca.wendyliu.spring5recipeapp.domain.Recipe;
-import ca.wendyliu.spring5recipeapp.repository.RecipeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 /**
  * Returns a list of recipes in database.
  */
-@Service
-public class RecipeService {
+public interface RecipeService {
 
-    // TODO: create interface for this
-
-    private RecipeRepository recipeRepository;
-
-    @Autowired
-    public RecipeService(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
-    }
-
-    public List<Recipe> getAllRecipes() {
-        ArrayList<Recipe> recipes = new ArrayList<>();
-        recipeRepository.findAll().forEach(recipes::add);
-
-        return recipes;
-    }
+    Set<Recipe> getAllRecipes();
 }
---
+++ src/main/java/ca/wendyliu/spring5recipeapp/service/RecipeServiceImpl.java
@@ -0,0 +1,30 @@
+package ca.wendyliu.spring5recipeapp.service;
+
+import ca.wendyliu.spring5recipeapp.domain.Recipe;
+import ca.wendyliu.spring5recipeapp.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Returns a list of recipes in database.
+ */
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    @Autowired
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public Set<Recipe> getAllRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
+}
