a8562b89a3f3644f83848b5b117a4676b997f2e4
Wendy Liu <jingyliu@paypal.com>
2018-01-02 11:46:34 -0800

    [master] Added logging

--- src/main/java/ca/wendyliu/spring5recipeapp/service/RecipeServiceImpl.java
+++ src/main/java/ca/wendyliu/spring5recipeapp/service/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package ca.wendyliu.spring5recipeapp.service;
 
 import ca.wendyliu.spring5recipeapp.domain.Recipe;
 import ca.wendyliu.spring5recipeapp.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,8 @@ import java.util.Set;
 /**
  * Returns a list of recipes in database.
  */
+// Gives us a logger
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -23,6 +26,9 @@ public class RecipeServiceImpl implements RecipeService {
     }
 
     public Set<Recipe> getAllRecipes() {
+        // enabled by @Slf4j
+        log.debug("I'm in the service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
