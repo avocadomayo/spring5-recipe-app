8f186081336e6e0a671ab3424880d31573456d01
Wendy Liu <jingyliu@paypal.com>
2018-01-02 11:45:53 -0800

    [master] Added logging

--- src/main/java/ca/wendyliu/spring5recipeapp/controller/IndexController.java
+++ src/main/java/ca/wendyliu/spring5recipeapp/controller/IndexController.java
@@ -1,12 +1,14 @@
 package ca.wendyliu.spring5recipeapp.controller;
 
 import ca.wendyliu.spring5recipeapp.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class IndexController {
 
     RecipeService recipeService;
@@ -18,6 +20,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("I'm in the controller!");
 
         model.addAttribute("recipes", recipeService.getAllRecipes());
 
