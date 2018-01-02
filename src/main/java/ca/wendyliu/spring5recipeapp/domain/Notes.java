1c17130b77e6ffcd1b2d7605b2c750a1ae6bcd85
Wendy Liu <jingyliu@paypal.com>
2018-01-02 11:46:17 -0800

    [master] Refactored using Lombok

--- src/main/java/ca/wendyliu/spring5recipeapp/domain/Notes.java
+++ src/main/java/ca/wendyliu/spring5recipeapp/domain/Notes.java
@@ -1,7 +1,11 @@
 package ca.wendyliu.spring5recipeapp.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
@@ -24,27 +28,4 @@ public class Notes {
         this.recipeNotes = recipeNotes;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
