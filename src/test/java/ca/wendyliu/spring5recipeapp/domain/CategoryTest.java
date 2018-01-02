3c7665764f7e27a50ec97e51182fa119e259bf74
Wendy Liu <jingyliu@paypal.com>
2018-01-02 11:47:17 -0800

    [master] Added tests

---
+++ src/test/java/ca/wendyliu/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,34 @@
+package ca.wendyliu.spring5recipeapp.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long id = 4L;
+        category.setId(id);
+        assertEquals(id, category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+        String description = "yummy";
+        category.setDescription(description);
+        assertEquals(description, category.getDescription());
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
