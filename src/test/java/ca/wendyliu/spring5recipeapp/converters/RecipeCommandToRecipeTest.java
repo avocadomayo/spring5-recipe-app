d0475a52c65f5e582e60d3b1ca6ca14a5bcfa50a
Wendy Liu <jingyliu@paypal.com>
2018-01-25 10:34:44 -0800

    [master] Added two extra tests

--- src/test/java/ca/wendyliu/spring5recipeapp/converters/RecipeCommandToRecipeTest.java
+++ src/test/java/ca/wendyliu/spring5recipeapp/converters/RecipeCommandToRecipeTest.java
@@ -97,4 +97,134 @@ public class RecipeCommandToRecipeTest {
         assertNotNull(recipe.getNotes());
         assertThat("size of categories set", recipe.getCategories().size(), is(categories.size()));
     }
+
+    @Test
+    public void convertWithNullIngredients() {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(ID);
+        command.setDescription(DESCRIPTION);
+        command.setPrepTime(PREP_TIME);
+        command.setCookTime(COOK_TIME);
+        command.setServings(SERVINGS);
+        command.setSource(SOURCE);
+        command.setUrl(URL);
+        command.setDirections(DIRECTIONS);
+        command.setDifficulty(DIFFICULTY);
+        command.setNotes(new NotesCommand());
+
+        CategoryCommand category1 = new CategoryCommand();
+        category1.setDescription("American");
+        CategoryCommand category2 = new CategoryCommand();
+        category2.setDescription("Mexican");
+        CategoryCommand category3 = new CategoryCommand();
+        category3.setDescription("Japanese");
+        HashSet<CategoryCommand> categories = new HashSet<>(Arrays.asList(category1, category2, category3));
+
+        command.setCategories(categories);
+
+        Recipe recipe = converter.convert(command);
+
+        assertNotNull(recipe);
+        assertEquals(ID, recipe.getId());
+        assertEquals(DESCRIPTION, recipe.getDescription());
+        assertEquals(PREP_TIME, recipe.getPrepTime());
+        assertEquals(COOK_TIME, recipe.getCookTime());
+        assertEquals(SERVINGS, recipe.getServings());
+        assertEquals(SOURCE, recipe.getSource());
+        assertEquals(URL, recipe.getUrl());
+        assertEquals(DIRECTIONS, recipe.getDirections());
+        assertThat("size of ingredients list", recipe.getIngredients().size(), is(0));
+        assertEquals(DIFFICULTY, recipe.getDifficulty());
+        assertNotNull(recipe.getNotes());
+        assertThat("size of categories set", recipe.getCategories().size(), is(categories.size()));
+    }
+
+    @Test
+    public void convertWithNullCategories() {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(ID);
+        command.setDescription(DESCRIPTION);
+        command.setPrepTime(PREP_TIME);
+        command.setCookTime(COOK_TIME);
+        command.setServings(SERVINGS);
+        command.setSource(SOURCE);
+        command.setUrl(URL);
+        command.setDirections(DIRECTIONS);
+
+        IngredientCommand ingredient1 = new IngredientCommand();
+        ingredient1.setDescription("avocado");
+        IngredientCommand ingredient2 = new IngredientCommand();
+        HashSet<IngredientCommand> ingredients = new HashSet<>(Arrays.asList(ingredient1, ingredient2));
+        ingredients.add(ingredient1);
+        ingredients.add(ingredient2);
+
+        command.setIngredients(ingredients);
+        command.setDifficulty(DIFFICULTY);
+        command.setNotes(new NotesCommand());
+
+        Recipe recipe = converter.convert(command);
+
+        assertNotNull(recipe);
+        assertEquals(ID, recipe.getId());
+        assertEquals(DESCRIPTION, recipe.getDescription());
+        assertEquals(PREP_TIME, recipe.getPrepTime());
+        assertEquals(COOK_TIME, recipe.getCookTime());
+        assertEquals(SERVINGS, recipe.getServings());
+        assertEquals(SOURCE, recipe.getSource());
+        assertEquals(URL, recipe.getUrl());
+        assertEquals(DIRECTIONS, recipe.getDirections());
+        assertThat("size of ingredient set", recipe.getIngredients().size(), is(ingredients.size()));
+        assertEquals(DIFFICULTY, recipe.getDifficulty());
+        assertNotNull(recipe.getNotes());
+        assertThat("size of categories set", recipe.getCategories().size(), is(0));
+    }
+
+    @Test
+    public void convertWithNullNotes() {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(ID);
+        command.setDescription(DESCRIPTION);
+        command.setPrepTime(PREP_TIME);
+        command.setCookTime(COOK_TIME);
+        command.setServings(SERVINGS);
+        command.setSource(SOURCE);
+        command.setUrl(URL);
+        command.setDirections(DIRECTIONS);
+
+        IngredientCommand ingredient1 = new IngredientCommand();
+        ingredient1.setDescription("avocado");
+        IngredientCommand ingredient2 = new IngredientCommand();
+        HashSet<IngredientCommand> ingredients = new HashSet<>(Arrays.asList(ingredient1, ingredient2));
+        ingredients.add(ingredient1);
+        ingredients.add(ingredient2);
+
+        command.setIngredients(ingredients);
+        command.setDifficulty(DIFFICULTY);
+
+        CategoryCommand category1 = new CategoryCommand();
+        category1.setDescription("American");
+        CategoryCommand category2 = new CategoryCommand();
+        category2.setDescription("Mexican");
+        CategoryCommand category3 = new CategoryCommand();
+        category3.setDescription("Japanese");
+        HashSet<CategoryCommand> categories = new HashSet<>(Arrays.asList(category1, category2, category3));
+
+        command.setCategories(categories);
+
+        Recipe recipe = converter.convert(command);
+
+        assertNotNull(recipe);
+        assertEquals(ID, recipe.getId());
+        assertEquals(DESCRIPTION, recipe.getDescription());
+        assertEquals(PREP_TIME, recipe.getPrepTime());
+        assertEquals(COOK_TIME, recipe.getCookTime());
+        assertEquals(SERVINGS, recipe.getServings());
+        assertEquals(SOURCE, recipe.getSource());
+        assertEquals(URL, recipe.getUrl());
+        assertEquals(DIRECTIONS, recipe.getDirections());
+        assertThat("size of ingredient set", recipe.getIngredients().size(), is(ingredients.size()));
+        assertEquals(DIFFICULTY, recipe.getDifficulty());
+        assertNull(recipe.getNotes());
+        assertThat("size of categories set", recipe.getCategories().size(), is(categories.size()));
+    }
 }
