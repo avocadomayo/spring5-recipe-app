92d0ae072a0176b62dae9da91ecadd4122abb810
Wendy Liu <jingyliu@paypal.com>
2018-01-26 14:48:27 -0800

    [master] Added a front-end recipe form to allow users to create new recipes

--- src/main/java/ca/wendyliu/spring5recipeapp/controller/RecipeController.java
+++ src/main/java/ca/wendyliu/spring5recipeapp/controller/RecipeController.java
@@ -1,9 +1,12 @@
 package ca.wendyliu.spring5recipeapp.controller;
 
+import ca.wendyliu.spring5recipeapp.commands.RecipeCommand;
 import ca.wendyliu.spring5recipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,23 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String addNewRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipe_form";
+    }
+
+    // both annotations are required, as we are doing a POST & a GET to the show recipe page
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        // this command tells Spring MVC to redirect to a specific URL.
+        // so this means after we save a recipe, redirect to show this new recipe.
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
